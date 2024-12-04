622a37a666bdff566dd281cb27534ff925157fe2
Szniobor <mszymonek13@gmail.com>
2024-12-04 20:02:33 +0100

    docs version i dont care

--- html/search/all_2.js
+++ html/search/all_2.js
@@ -3,8 +3,9 @@ var searchData=
   ['clk_0',['CLK',['../group___configuration.html#ga4b355291fe6b8ba8e167ab0faa862e45',1,'defines.h']]],
   ['clock_1',['Clock',['../struct_clock.html',1,'']]],
   ['clock_5fpriority_2',['CLOCK_PRIORITY',['../group___configuration.html#gabdd9605a9848e42394a214f81b8d3b11',1,'defines.h']]],
-  ['clockhandle_3',['clockHandle',['../main_8c.html#ad36be1a106dac0133202491b6a19c0ee',1,'main.c']]],
-  ['condition_5fcheck_5finterval_5fms_4',['CONDITION_CHECK_INTERVAL_MS',['../group___configuration.html#gaf9cfe55697c1f0d57740572bd50e6cda',1,'defines.h']]],
-  ['core_5',['CORE',['../group___configuration.html#gaaa2a64e1806a86f073e15277d94cb94a',1,'defines.h']]],
-  ['cs_6',['CS',['../group___configuration.html#ga3780e2fe762dc532df7d0f030b55caa0',1,'defines.h']]]
+  ['clock_5fstack_3',['CLOCK_STACK',['../group___configuration.html#ga4ab7dc9545189c0d83e4bb00abcf8d3c',1,'defines.h']]],
+  ['clockhandle_4',['clockHandle',['../main_8c.html#ad36be1a106dac0133202491b6a19c0ee',1,'main.c']]],
+  ['condition_5fcheck_5finterval_5fms_5',['CONDITION_CHECK_INTERVAL_MS',['../group___configuration.html#gaf9cfe55697c1f0d57740572bd50e6cda',1,'defines.h']]],
+  ['core_6',['CORE',['../group___configuration.html#gaaa2a64e1806a86f073e15277d94cb94a',1,'defines.h']]],
+  ['cs_7',['CS',['../group___configuration.html#ga3780e2fe762dc532df7d0f030b55caa0',1,'defines.h']]]
 ];
